58d1b79f1c97c4b6807fdb0e97cbc65a11177c58
burcusevinc <sburcusevinc@gmail.com>
2023-02-13 02:19:09 +0300

    koa server ödev 6

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const {circleArea, circleCircumference} = require('./circle')
-circleArea(5)
-circleCircumference(5)
+const Koa = require('koa');
+const app = new Koa();
+
+// response
+app.use(ctx => {
+    if (ctx.path == '/') {
+        ctx.body = '<h1>index sayfasina hosgeldiniz</h1>';
+    }
+    else if (ctx.path == '/hakkimda') {
+        ctx.body = '<h1>hakkimda sayfasina hosgeldiniz</h1>';
+    }
+    else if (ctx.path == '/iletisim') {
+        ctx.body = '<h1>iletisim sayfasina hosgeldiniz</h1>';
+    }
+    console.log(ctx.path)
+});
+
+app.listen(3000);
